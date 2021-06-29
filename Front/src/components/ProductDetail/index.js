b5ec234ee1df479d79589403097ca82c9d00a54b
belenbuccolo <belenbuccolo@gmail.com>
2021-06-29 18:10:02 -0300

    agregado un componente de detalle de productos y otro para landing page incompletos

---
+++ Front/src/components/ProductDetail/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ProductDetail = function () {
+  return (
+    <div>
+      <h2>Banana</h2>
+      <img src="" alt="" />
+      <p>Detalles</p>
+      <button>+ ADD</button>
+    </div>
+  );
+};
+
+export default ProductDetail;
