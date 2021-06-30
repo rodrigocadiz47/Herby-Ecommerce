6a52d912555ee00d3540ca5afb3a567f40ca2c25
erikazurmuhle <erikadzurmuhle@gmail.com>
2021-06-30 19:54:44 -0300

    rutas order

--- Back/routes/users.js
+++ Back/routes/users.js
@@ -1,27 +1,25 @@
-const express = require("express")
-const router = express.Router()
-const passport = require("passport")
-const {User} = require("../Models")
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
+const { User } = require("../Models");
 
-router.get("/", (req, res, next)=>{
-    User.findAll()
-    .then((users)=>{
-        res.send(users)
-    })
-})
+router.get("/", (req, res, next) => {
+  User.findAll().then((users) => {
+    res.send(users);
+  });
+});
 
-router.get("/me", (req, res, next)=>{
-    res.send(req.user)
-})
+router.get("/me", (req, res, next) => {
+  res.send(req.user);
+});
 
 //POST METHOD
-router.post("/", (req, res, next)=>{
-    User.create(req.body)
-    .then(user=> res.send(user))
-})
+router.post("/register", (req, res, next) => {
+  User.create(req.body).then((user) => res.send(user));
+});
 
-router.post("/login", passport.authenticate("local"), (req, res, next)=>{
-    res.send(req.user)
-})
+router.post("/login", passport.authenticate("local"), (req, res, next) => {
+  res.send(req.user);
+});
 
-module.exports= router
+module.exports = router;
