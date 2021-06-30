0663269138cf0a7d884556b9b48977106501d231
rodrigocadiz47 <rodrigocadiz47@gmail.com>
2021-06-30 15:26:37 -0300

    agregado logging false

--- Back/Models/PurchaseOrder.js
+++ Back/Models/PurchaseOrder.js
@@ -4,14 +4,14 @@ const db = require("./_db")
 class PurchaseOrder extends S.Model {}
 PurchaseOrder.init(
   {
-    deliveryRange: {
-      type: S.STRING,
-      allowNull: false,
-    },
-    wayToPay: {
-      type: S.STRING,
-      allowNull: false,
-    },
+    // deliveryRange: {
+    //   type: S.STRING,
+    //   allowNull: false,
+    // },
+    // wayToPay: {
+    //   type: S.STRING,
+    //   allowNull: false,
+    //},
     total:{
       type: S.INTEGER
     }
--- Back/Models/_db.js
+++ Back/Models/_db.js
@@ -11,6 +11,7 @@ const { Sequelize } = require("sequelize");
 const db = new Sequelize("herby", null, null, {
   host: "localhost",
   dialect: "postgres",
+  logging: false
 });
 db.sync({force: false}).then(()=>console.log("DB CONNECTED"))
 
