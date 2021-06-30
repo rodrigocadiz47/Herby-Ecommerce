e36b9f1301340693e6e7b2dc486874f6a2366afd
belenbuccolo <belenbuccolo@gmail.com>
2021-06-29 21:39:50 -0300

    antes de empezar a probar los estilos

---
+++ Front/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["./Front/src/**/*.{js,jsx}", "./Front/public/index.html"], // paths to all componets
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
