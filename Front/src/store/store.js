bdd0178b6a75327e5201f936c987aeba4107005d
belenbuccolo <belenbuccolo@gmail.com>
2021-06-30 19:21:12 -0300

    agregamos productsReducer y modificamos productsContainer para que use redux

--- Front/src/store/store.js
+++ Front/src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import usersReducer from "./usersReducer";
+import productsReducer from "./productsReducer";
 
 const store = configureStore({
   reducer: {
     users: usersReducer,
+    products: productsReducer,
   },
 });
 
