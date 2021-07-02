1150bc62de9cfe45232512eebf89596568f9ca15
rodrigocadiz47 <rodrigocadiz47@gmail.com>
2021-07-01 21:07:22 -0300

    acoplando main

--- Back/routes/order.js
+++ Back/routes/order.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { Order } = require("../Models");
+const { Order, User } = require("../Models");
 
 router.get("/pending/:id", (req, res, next) => {
   const userId = req.params.id;
@@ -16,23 +16,43 @@ router.get("/pending/:id", (req, res, next) => {
 
 router.post("/:id", (req, res, next) => {
   const userId = req.params.id;
-  const { name, amount, price, id, preTotal } =
-    req.body;
-  return Order.create({
-    productName: name,
-    productQuantity: amount,
-    productPrice: price,
-    productId: id,
-    totalOrder: preTotal,
-  })
-    .then((order) => {
-      order.setUser(userId).then(() => {
-        Products.findByPk(id).then((product) => {
-          res.send(product);
+  const { name, amount, price, id, preTotal } = User.findByPk(userId)
+    .then((user) => {
+      if (user.firstName) {
+        return Order.create({
+          productName: name,
+          productQuantity: amount,
+          productPrice: price,
+          productId: id,
+          totalOrder: preTotal,
+        }).then((order) => {
+          order
+            .setUser(userId)
+            .then(() => Products.findByPk(id))
+            .then((product) => {
+              res.send(product);
+            })
+            .catch((error) => res.status(400).send(error));
         });
-      });
+      }
     })
-    .catch((error) => res.status(400).send(error));
+    .catch((error) => res.status(404).send(error));
+});
+
+router.put("/:id", (req, res, next) => {
+  const { productQuantity } = req.body;
+  const orderId = req.params.id;
+  Order.findByPk(orderId).then((order) => {
+    if (order.id) {
+      order
+        .update({
+          productQuantity,
+        })
+        .then((order) => {
+          res.send(order);
+        });
+    }
+  });
 });
 
 router.delete("/:id", (req, res, next) => {
