4db494f48c2a18d0f25cc212ed9d67382dd50f68
belenbuccolo <belenbuccolo@gmail.com>
2021-07-06 16:57:13 -0300

    nuevos componentes para admin

--- Front/src/store/productsReducer.js
+++ Front/src/store/productsReducer.js
@@ -10,6 +10,10 @@ export const GET_PRODUCTS = createAsyncThunk("GET_PRODUCTS", (pathname) => {
   return axios.get(`http://localhost:3001/api${pathname}`).then((res) => res.data);
 });
 
+export const ADD_PRODUCT = createAsyncThunk("ADD_PRODUCT", (product) => {
+  return axios.post("http://localhost:3001/api/products/admin", product);
+});
+
 const productsReducer = createReducer(initialState, {
   [GET_PRODUCTS.fulfilled]: (state, action) => {
     state.products = action.payload;
@@ -17,6 +21,12 @@ const productsReducer = createReducer(initialState, {
   },
   [GET_PRODUCTS.rejected]: (state, action) => {
     state.error = true;
+  },
+  [ADD_PRODUCT.fulfilled]: (state, action) => {
+    state.products = [...state.products, action.payload];
+  },
+  [ADD_PRODUCT.rejected]: (state, action) => {
+    state.error = action.error;
   },
 });
 
