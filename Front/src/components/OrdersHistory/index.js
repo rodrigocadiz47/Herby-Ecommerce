c4c1f74e3c3aeea25213304d6fc732036b9bd447
belenbuccolo <belenbuccolo@gmail.com>
2021-07-06 13:06:52 -0300

    historial de ordenes con vista

--- Front/src/components/OrdersHistory/index.js
+++ Front/src/components/OrdersHistory/index.js
@@ -6,19 +6,57 @@ import { GET_HISTORY } from "../../store/usersReducer";
 const OrdersHistory = function () {
   const dispatch = useDispatch();
   const user = useSelector((store) => store.users.currentUser);
-  const ordersHistory = useSelector((store) => store.users.history);
+  const ordersHistory = useSelector((store) => store.users.ordersHistory);
 
   useEffect(() => {
     dispatch(GET_HISTORY(user.id));
+    console.log("ordersHistory", ordersHistory);
   }, [dispatch, user]);
 
   return (
-    <div>
-      <h1>History</h1>
-      {ordersHistory && ordersHistory.map((purchaseOrder) => (
-        <p>{purchaseOrder.id}</p>
-      ))}
-    </div>
+    <section className="text-gray-600 body-font overflow-hidden">
+      <div className="container px-5 py-24 mx-auto">
+        <h1 className="font-lora text-gray-900 text-3xl title-font font-medium mb-8">
+          Mis Ordenes
+        </h1>
+
+        {ordersHistory &&
+          ordersHistory.map((purchaseOrder) => {
+            return (
+              <div className="my-5">
+                <div className="flex flex-row justify-between bg-white rounded p-4 border">
+                  <div>
+                    <h2>Order ID: {purchaseOrder.id}</h2>
+                    <p>Order time: {purchaseOrder.createdAt.substring(0, 10)}</p>
+                  </div>
+                  <p className="text-xl">${purchaseOrder.total}</p>
+                </div>
+                <div>
+                  {purchaseOrder.orders.map((order) => {
+                    return (
+                      <div className="flex flex-row justify-between bg-gray-100 rounded p-4 border">
+                        <div>
+                          <p>{order.product.name}</p>
+                          <img
+                            alt=""
+                            src={order.product.image}
+                            className="h-16 rounded border my-2"
+                          ></img>
+                        </div>
+                        <p className="text-md">${order.product.price}</p>
+                        {/* {order.product.map((product) => {
+                        <p>{product.name}</p>;
+                      })} */}
+                        {console.log(order.product)}
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            );
+          })}
+      </div>
+    </section>
   );
 };
 
