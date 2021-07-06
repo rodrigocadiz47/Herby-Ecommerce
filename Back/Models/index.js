2fafe0390c8f11cb428c561429dc8b5078d68b1c
belenbuccolo <belenbuccolo@gmail.com>
2021-07-06 13:12:31 -0300

    vista de historial

--- Back/Models/index.js
+++ Back/Models/index.js
@@ -4,15 +4,19 @@ const Order = require("./Order");
 const PurchaseOrder = require("./PurchaseOrder");
 const Products = require("./Products");
 
+// One to many, userId en purchaseOrder
 PurchaseOrder.belongsTo(User);
 User.hasMany(PurchaseOrder);
 
+// One to many, purchaseOrderId en Order
 Order.belongsTo(PurchaseOrder);
 PurchaseOrder.hasMany(Order);
 
+// One to many, productId en Order
 Order.belongsTo(Products);
 Products.hasMany(Order);
 
+// One to many, userId en Order
 Order.belongsTo(User);
 User.hasMany(Order);
 
