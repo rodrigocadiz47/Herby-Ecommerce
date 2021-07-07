19b38ad3b261bb465825ef152deb8314877934b7
HernanLanger <hernanlanger@gmail.com>
2021-07-07 16:50:05 -0300

    debug carrito

--- Front/src/containers/LoginContainer/index.js
+++ Front/src/containers/LoginContainer/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-anonymous-default-export */
 import axios from "axios";
 import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
@@ -21,20 +22,30 @@ export default () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    dispatch(SET_USER(loginData)).then((user) => {
-      if (cart.length) {
-        cart.forEach((order) => {
-          axios.post(
-            `http://localhost:3001/api/orders/${user.payload.id}`,
-            order
-          );
-        });
-      }
-      dispatch(GET_LOG_CART(user.payload.id))
-      if (user.payload) {
-        history.push("/home");
-      }
-    });
+    dispatch(SET_USER(loginData))
+      .then((user) => {
+        if (cart.length) {
+          cart.forEach((order) => {
+            axios.post(
+              `http://localhost:3001/api/orders/${user.payload.id}`,
+              order
+            );
+          });
+        }
+        return user.payload;
+        //  else {
+        //   dispatch(GET_LOG_CART(user.payload.id));
+        //   if (user.payload) {
+        //     history.push("/home");
+        //   }
+        // }
+      })
+      .then((user) => {
+        dispatch(GET_LOG_CART(user.id));
+        if (user.id) {
+          history.push("/home");
+        }
+      });
   };
 
   return (
@@ -43,3 +54,8 @@ export default () => {
     </div>
   );
 };
+
+// 1) traer orders pending
+// 2) localstore.map( order =>
+//                            if(cart.filter (cOrder.id === order.id).length){edit_amount}else{create_order})
+//
